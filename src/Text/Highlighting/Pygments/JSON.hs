cbccf4e25ec16b1e5e64b490111286311d2f81f3
David Lazar <lazar6@illinois.edu>
2012-08-15 19:14:09 -0500

    Run the JSON dumper script by invoking 'python' directly. The previous approach was problematic because Cabal does not install the script with execute permissions.

--- src/Text/Highlighting/Pygments/JSON.hs
+++ src/Text/Highlighting/Pygments/JSON.hs
@@ -18,10 +18,12 @@ getJSONDumper = getDataFileName "pygments_dump_json.py"
 getPygmentsJSON :: FromJSON a => String -> IO a
 getPygmentsJSON what = do
     jsonDumper <- getJSONDumper
-    (exitCode, stdout, _) <- readProcessWithExitCode jsonDumper [what] ""
+    (exitCode, stdout, _) <- runPython jsonDumper [what] ""
     when (exitCode /= ExitSuccess) $
         fail $ jsonDumper ++ " failed: " ++ show exitCode
     case decode stdout of
         Nothing -> fail $ "failed to decode " ++ what
         Just ls -> return ls
-
+  where
+    runPython script args input = do
+        readProcessWithExitCode "python" (script : args) input
